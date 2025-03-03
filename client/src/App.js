05e5ab7bfc71c699eb58b1272a1447aae218af4c
Gayle Hoefer <gaylehoefer@gmail.com>
2025-03-03 01:09:43 -0700

    It is running without tailwind errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import './index.css';
 import Pizzas from "./pages/Pizzas";
 import Toppings from "./pages/Toppings";
 
