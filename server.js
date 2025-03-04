1c07aea67ac527a224a24dc5008c59fb25024861
Gayle Hoefer <gaylehoefer@gmail.com>
2025-03-04 00:15:52 -0700

    website is working

--- server.js
+++ server.js
@@ -19,8 +19,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 5000;
-
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -40,4 +38,6 @@ app.get("/", (req, res) => {
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running on port ${PORT}"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
